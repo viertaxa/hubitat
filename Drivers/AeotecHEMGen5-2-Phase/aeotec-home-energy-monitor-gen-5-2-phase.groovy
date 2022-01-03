855f46b0e3ac6c55fa8a786ddb6d8714f212dce0
Mark E. Scott, Jr <mscott@scott-usa.com>
2022-01-02 21:37:33 -0600

    Fix parameter for multiChannelAssociationV3.multiChannelAssociationRemove

--- Drivers/AeotecHEMGen5-2-Phase/aeotec-home-energy-monitor-gen-5-2-phase.groovy
+++ Drivers/AeotecHEMGen5-2-Phase/aeotec-home-energy-monitor-gen-5-2-phase.groovy
@@ -412,7 +412,7 @@ def setAssociations() {
     logTrace "Iterating on multiChannelAssociations"
     state.multiChannelAssociations[1].each { node ->
         logTrace "Removing node: $node"
-        runCommand(zwave.multiChannelAssociationV3.multiChannelAssociationRemove(groupingIdentifier: 1, nodeId: node))
+        runCommand(zwave.multiChannelAssociationV3.multiChannelAssociationRemove(groupingIdentifier: 1, nodeId: [node]))
         pauseExecution(1)
     }
     logTrace "Refreshing Associations"
